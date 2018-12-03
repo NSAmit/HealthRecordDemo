529e331aa18f1712eb3599682e6bd42321719b36
Amit Rote <rote12amit@me.com>
2018-12-02 18:09:27 -0800

    Initial Commit

---
+++ HealthAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthAppDemo
+//
+//  Created by Amit C Rote on 12/2/18.
+//  Copyright © 2018 Amit C Rote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
